59239a29bbca0d221235fda2ea16deba9ecc5b8b
nazieagle <nazeagle@163.com>
2015-06-27 17:08:51 +0800

    课堂练习

--- test.js
+++ test.js
@@ -1,4 +1,28 @@
-/**
- * Created by Olive.C on 2015/6/27.
- */
-console.log('Hello World');
+var http=require('http');
+
+var server=http.createServer(function (req, res) {
+    res.writeHead(200,{'Content-Type':'text/html;charset=utf-8'})
+    var url=req.url;
+
+    var urlObj={};
+    if(url.indexOf('?')>0){
+        var urls=url.split('?');
+        var pathname = urls[0];
+        var queries=urls[1].split('&');
+        queries.forEach(function (query) {
+            var q=query.split('=');
+            urlObj[q[0]]=q[1];
+        })
+
+    }
+
+    if(pathname=='/menu'){
+        res.end('菜单');
+    }else if(pathname == '/leg'){
+        res.end('客官，鸡腿饭'+urlObj['num']+'份来了 。');
+    }
+
+});
+server.listen(3000,'localhost', function () {
+    console.log('server listening')
+});
